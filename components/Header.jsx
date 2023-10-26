eecaea9584d31405b45be54cf3d4263b6555a260
Charles Lee Lin Ta <e0484321@u.nus.edu>
2023-10-26 12:17:25 +0800

    Update image path

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <div className="header">
-            <img src="../images/troll-face.png" className="troll-face"/>
+            <img src="/meme-generator/images/troll-face.png" className="troll-face"/>
             <div className="left-text">Meme Generator</div>
             <div className="right-text">React Course - Project 3</div>
         </div>
