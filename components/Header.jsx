b958a4df1a3de6b178613d80c1dc8c2f96503e66
Charles Lee Lin Ta <e0484321@u.nus.edu>
2023-11-05 23:40:02 +0800

    Update to absolute path

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <div className="header">
-            <img src="/meme-generator/images/troll-face.png" className="troll-face"/>
+            <img src="https://github.com/CharlesLee01/meme-generator/blob/main/images/troll-face.png?raw=true" className="troll-face"/>
             <div className="left-text">Meme Generator</div>
             <div className="right-text">React Course - Project 3</div>
         </div>
